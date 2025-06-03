51d77d25bc40335e34f570a6b5d82d47820c3029
0413w <0413wayne@gmail.com>
2025-06-03 23:59:59 +0800

    正式版ppt-雲端連結

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 document.getElementById("interact-btn").addEventListener("click", () => {
     alert("桌寵：嗨嗨～你想跟我做什麼呢？ヽ(・∀・)ﾉ");
 
- // 下載桌寵檔案
-const link = document.createElement("a");
-link.href = ".idea.zip"; // 替換成你的檔案路徑
-link.download = "my-desktop-pet.zip";   // 下載後的檔名
-document.body.appendChild(link);
-link.click();
-document.body.removeChild(link);
-    // 打開新網址
-    window.open("https://drive.google.com/file/d/1sllTV_VxOg_jqWG2F4G0seN4OvPnFy0e/view?usp=sharing", "_blank");
+    // 開啟 Google Drive 分享連結
+    window.open(
+        "https://drive.google.com/file/d/1sllTV_VxOg_jqWG2F4G0seN4OvPnFy0e/view?usp=sharing",
+        "_blank"
+    );
 });
+
+window.onload = function () {
+    const bgm = document.getElementById("bgm");
+    bgm.play().catch(() => {
+        console.log("請點擊頁面以啟動音樂");
+    });
+};
